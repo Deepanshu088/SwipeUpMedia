9ab607a6e8628a097449ead42a747d93ed251f1a
Deepanshu088 <deepanshujain088@gmail.com>
2021-03-22 18:01:19 +0530

    First Publish

--- controllers/adminRoute.js
+++ controllers/adminRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
 const router = express.Router();
 const mongoose = require("mongoose");
 const Blog = require('../models/blogSchema')
